f6e13746704305c2b62748f6e734046036a1568b
dmfdiogo <dmf.diogo@gmail.com>
2024-06-11 16:55:04 -0300

    Add project files.

---
+++ aws-ec2-docker-aspnet-webapi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace aws_ec2_docker_aspnet_webapi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
